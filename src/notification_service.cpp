d98d0403a10d6c3c576cfa440ca5bc2478e499c7
arslee07 <mail@arslee.me>
2026-06-23 11:37:41 +0300

    refactor: remove useless empty check; do not re-insert an entry

--- src/notification_service.cpp
+++ src/notification_service.cpp
@@ -105,20 +105,16 @@ bool NotificationService::fail(std::string_view id, std::int64_t now) {
         }
 
         auto pending_it = it->second;
-        shard.notifications.erase(it);
-
         auto node = shard.pendings.extract(pending_it);
-        if (!node.empty()) {
-            node.value().attempts += 1;
 
-            int limited_attempts = std::min(node.value().attempts, 30);
-            std::int64_t delay = 1LL << (limited_attempts - 1);
-            node.value().send_at = now + delay;
-            node.value().status = NotificationStatus::Pending;
+        node.value().attempts += 1;
+        int limited_attempts = std::min(node.value().attempts, 30);
+        std::int64_t delay = 1LL << (limited_attempts - 1);
+        node.value().send_at = now + delay;
+        node.value().status = NotificationStatus::Pending;
 
-            auto insert_res = shard.pendings.insert(std::move(node));
-            shard.notifications[insert_res.position->id] = insert_res.position;
-        }
+        auto insert_res = shard.pendings.insert(std::move(node));
+        it->second = insert_res.position;
     }
 
     return true;
